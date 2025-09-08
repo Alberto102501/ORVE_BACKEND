9b628f70703b87283aa9e2e013ba49d21aafc290
Alberto102501 <alibarrasa@ittepic.edu.mx>
2025-09-08 12:25:03 -0700

    Conexion con mongo creada

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const userRoutes = require('./src/routes/user.route');
+const connectDB = require('./db');
 
 const app = express();
 
+app.use(express.json());
+
+app.use('/users', userRoutes);
+
 const PORT = process.env.PORT || 3000;
 
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
