c973e48b4b67b30e9e174c6e678f865cb11cbfb5
Alberto102501 <alibarrasa@ittepic.edu.mx>
2025-09-11 12:48:26 -0700

    Comente la validacion

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,7 +4,7 @@ const validateToken = require('../middleware/validateToken')
 
 const Router = express.Router();
 
-Router.get('/', validateToken.authRequired, userController.getUsers);
+Router.get('/', /*validateToken.authRequired,*/ userController.getUsers);
 Router.post('/', userController.createUser);
 Router.put('/:id', userController.updateUser);
 Router.delete('/:id', userController.deleteUser);
