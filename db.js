f2973050d665199a65e0f5f7ae9bc7dda375fe07
DiegoMoreno7 <diegomorenoduran1@gmail.com>
2025-09-16 18:53:43 -0700

    Cambie la sintaxis de la  conexión a la bd a una versión mas reciente

--- db.js
+++ db.js
@@ -6,10 +6,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Conexión establecida con MongoDB Atlas');
   } catch (err) {
     console.error('Error al conectar con MongoDB Atlas:', err);
