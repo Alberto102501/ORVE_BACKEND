772e153111cd62b8a6ca9e552fd1ea60870c7431
Alberto102501 <alibarrasa@ittepic.edu.mx>
2025-10-06 13:31:30 -0700

    Subir excel

--- src/controllers/fuel/load.controller.js
+++ src/controllers/fuel/load.controller.js
@@ -1,9 +1,55 @@
 const newLoad = require('../../models/fuel/load.model');
 
 exports.createLoad = async(req, res) => {
+    const fieldMap = {
+        "Tipo producto": "productType",
+        "Cuenta": "account",
+        "Tarjeta/Tag": "cardOrTag",
+        "Placa": "licensePlate",
+        "Fecha": "date",
+        "ID": "transactionId",
+        "Cargo": "debit",
+        "Abono": "credit",
+        "Saldo": "balance",
+        "Tipo": "transactionType",
+        "Usuario": "user",
+        "Expediente": "caseFile",
+        "Concepto": "concept",
+        "Producto": "product",
+        "Precio": "price",
+        "Km INI": "kmStart",
+        "Km FIN": "kmEnd",
+        "Litros": "liters",
+        "Rendimiento": "efficiency",
+        "IVA": "iva",
+        "Estación": "station",
+        "Estación RFC": "stationRFC",
+        "Estado": "state",
+        "Ciudad": "city",
+        "Colonia": "neighborhood",
+        "Domicilio": "address",
+        "Año": "year",
+        "Mes": "month",
+        "Dia": "day",
+        "Hora": "hour"
+    };
+
     try {
         const payload = Array.isArray(req.body) ? req.body : [req.body];
-        const savedLoad = await newLoad.insertMany(payload);
+        const transformData = (rawData) => {
+            return rawData.map((row) => {
+                const transformed = {};
+                for (const key in row) {
+                const mappedKey = fieldMap[key];
+                if (mappedKey) {
+                    transformed[mappedKey] = row[key];
+                }
+                }
+                return transformed;
+            });
+        };
+        const transformedPayload = transformData(payload);
+        const savedLoad = await newLoad.insertMany(transformedPayload);
         res.status(201).json({message: 'Carga creada exitosamente', data: savedLoad});
     }catch (error) {
         res.status(500).json({ message: error.message });
