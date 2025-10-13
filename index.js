f8200722e7057674b6499dec32f6c779cf08328f
Alberto102501 <alibarrasa@ittepic.edu.mx>
2025-10-13 12:45:18 -0700

    limite de peso

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require('path');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
 app.use(morgan('dev'));
 app.use(cookieParser());
 
