d8495498e8c65ff860e51ed96e262ffb9c339803
DiegoMoreno7 <diegomorenoduran1@gmail.com>
2025-11-18 19:44:11 -0700

    Optimización de bitacora

--- src/routes/fuel/infoAdditional.route.js
+++ src/routes/fuel/infoAdditional.route.js
@@ -1,11 +1,15 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getInfos, getInfoByCardNumber, postInfo, putInfoByCardNumber } = require('../../controllers/fuel/infoAdditional.controller');
+const { getInfos, getInfoByCardNumber, postInfo, putInfoByCardNumber, getProcessedFuelInfo  } = require('../../controllers/fuel/infoAdditional.controller');
 
 router.route('/')
     .get(getInfos)
     .post(postInfo);
+
+// en tu archivo de rutas (ej: src/routes/fuel.routes.js)
+router.get('/processed', getProcessedFuelInfo);
+
 
 router.route('/:numberCard')
     .get(getInfoByCardNumber)
