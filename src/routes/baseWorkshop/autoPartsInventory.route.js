f4bd0139d6ad7dd2072152fd95c2ff74bc285755
DiegoMoreno7 <diegomorenoduran1@gmail.com>
2025-12-11 12:17:52 -0700

    token en taller de base

--- src/routes/baseWorkshop/autoPartsInventory.route.js
+++ src/routes/baseWorkshop/autoPartsInventory.route.js
@@ -7,17 +7,17 @@ const {
     deleteInventoryItem,
     updateInventoryQuantity
 } = require('../../controllers/baseWorkshop/autoPartsInventory.controller.js');
-const { authRequired } = require('../../middleware/validateToken.js');
+const validateToken = require('../../middleware/validateToken.js');
 const auditLogger = require('../../middleware/auditLogger.js');
 const AutoPartsInventory = require('../../models/baseWorkshop/autoPartsInventory.model.js');
 
 const router = Router();
 
-router.get('/', getInventory);
-router.post('/', auditLogger(AutoPartsInventory), createInventoryItem);
-router.get('/:id', getInventoryItem);
-router.put('/:id', auditLogger(AutoPartsInventory), updateInventoryItem);
-router.patch('/deduct-stock', auditLogger(AutoPartsInventory), updateInventoryQuantity);
-router.delete('/:id', auditLogger(AutoPartsInventory), deleteInventoryItem);
+router.get('/', validateToken.authRequired, getInventory);
+router.post('/', validateToken.authRequired, auditLogger(AutoPartsInventory), createInventoryItem);
+router.get('/:id', validateToken.authRequired, getInventoryItem);
+router.put('/:id', validateToken.authRequired, auditLogger(AutoPartsInventory), updateInventoryItem);
+router.patch('/deduct-stock', validateToken.authRequired, auditLogger(AutoPartsInventory), updateInventoryQuantity);
+router.delete('/:id', validateToken.authRequired, auditLogger(AutoPartsInventory), deleteInventoryItem);
 
 module.exports = router;
--- src/routes/baseWorkshop/requestProducts.route.js
+++ src/routes/baseWorkshop/requestProducts.route.js
@@ -2,12 +2,12 @@ const express = require('express');
 const requestProductsController = require('../../controllers/baseWorkshop/requestProducts.controller.js');
 const auditLogger = require('../../middleware/auditLogger.js');
 const RequestProducts = require('../../models/baseWorkshop/requestProducts.model.js');
-
+const validateToken = require('../../middleware/validateToken.js');
 const router = express.Router();
 
-router.get('/', requestProductsController.getAllRequestProducts);
-router.get('/getByFolio/:folio', requestProductsController.getAllRequestProductsByFolio);
-router.post('/createRequestProducts', auditLogger(RequestProducts), requestProductsController.createRequestProducts);
-router.patch('/updateStatus/:id', auditLogger(RequestProducts), requestProductsController.updateRequestProductsStatus);
+router.get('/', validateToken.authRequired, requestProductsController.getAllRequestProducts);
+router.get('/getByFolio/:folio', validateToken.authRequired, requestProductsController.getAllRequestProductsByFolio);
+router.post('/createRequestProducts', validateToken.authRequired, auditLogger(RequestProducts), requestProductsController.createRequestProducts);
+router.patch('/updateStatus/:id', validateToken.authRequired, auditLogger(RequestProducts), requestProductsController.updateRequestProductsStatus);
 
 module.exports = router;
